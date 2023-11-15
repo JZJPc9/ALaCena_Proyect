a0ce110edd4b53ec3b29d681e2bc003b80b61bdf
JZJPc9 <99989355+JZJPc9@users.noreply.github.com>
2023-11-15 13:56:52 -0600

    cocaina

--- app/src/main/java/com/example/alacena/ElecRol.java
+++ app/src/main/java/com/example/alacena/ElecRol.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class ElecRol extends AppCompatActivity {
-
+/*prueba de sobreescritura*/
 
     Button btnIng;
     Button btnCre;
