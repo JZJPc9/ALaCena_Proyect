31a87fca095b39952641beec96e01f8cdafe094e
JZJPc9 <99989355+JZJPc9@users.noreply.github.com>
2023-11-15 13:59:33 -0600

    prueba

--- app/src/main/java/com/example/alacena/ElecRol.java
+++ app/src/main/java/com/example/alacena/ElecRol.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class ElecRol extends AppCompatActivity {
-/*prueba de sobreescritura*/
+/*prueba de sobreescritura 2*/
 
     Button btnIng;
 
