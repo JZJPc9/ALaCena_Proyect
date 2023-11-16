1ea7b3e5f06bead6db86adbd1017c4f2a983232a
JZJPc9 <99989355+JZJPc9@users.noreply.github.com>
2023-11-16 01:50:05 -0600

    valRyL
    Validacion de campos de registro y login

--- app/src/main/java/com/example/alacena/Login.java
+++ app/src/main/java/com/example/alacena/Login.java
@@ -4,39 +4,82 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
-    ImageButton btnBack;
 
+
+    EditText correo,contrasena;
+    ImageButton btnBack;
     Button btnLog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //asociacion de los elementos de el fontend con el backend
+
+        // Asociación de los elementos de frontend con el backend
         btnBack = findViewById(R.id.btnBack);
         btnLog = findViewById(R.id.btnLog);
+        contrasena=findViewById(R.id.editTextPassword);
+        correo = findViewById(R.id.editTextEmailAddress);
 
-        //boton back
+        // Botón back
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Logica de el boton
+                // Lógica del botón
                 finish();
             }
         });
+
         btnLog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Logica de el boton
-                Intent irmenu = new Intent(getApplicationContext(),Menuprin.class);
-                startActivity(irmenu);
+                // Lógica del botón
+                if (!isValidEmail(correo.getText().toString())) {
+                    // Mostrar Toast si no es un formato válido de correo
+                    Toast.makeText(Login.this, "Formato de correo inválido", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Enviar datos y cambiar de actividad
+                    Intent irmenu = new Intent(getApplicationContext(), Menuprin.class);
+                    startActivity(irmenu);
+                }
             }
         });
+
+        // Agrega un TextWatcher al EditText
+        correo.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int before, int count) {
+                // No se necesita acción antes de cambiar el texto
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                // No se necesita acción durante el cambio del texto
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // Verifica si el texto contiene un arroba
+                if (!editable.toString().contains("@")) {
+                    // Si no contiene un arroba, agrégalo al final
+                }
+            }
+        });
+    }
+
+    // Función para verificar el formato de correo electrónico
+    private boolean isValidEmail(String email) {
+        // Puedes implementar aquí una validación más completa para el formato de correo
+        return email.contains("@");
     }
 }
--- app/src/main/java/com/example/alacena/Registro.java
+++ app/src/main/java/com/example/alacena/Registro.java
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Registro extends AppCompatActivity {
 
-
+    EditText correo,contrasena,confContrasena;
+    TextInputLayout nombre, apellidop, apellidom;
     ImageButton btnBack;
     Button btnReg;
 
@@ -21,6 +26,12 @@ public class Registro extends AppCompatActivity {
         //asociacion de los elementos de el fontend con el backend
         btnBack = findViewById(R.id.btnBack);
         btnReg = findViewById(R.id.Registro);
+        nombre = findViewById(R.id.textInputName);
+        apellidop = findViewById(R.id.textInputPat);
+        apellidom = findViewById(R.id.textInputMat);
+        contrasena = findViewById(R.id.editTextPassword);
+        confContrasena = findViewById(R.id.editTextConfPassword);
+        correo =findViewById(R.id.editTextEmailAddress);
 
 
         //boton back
@@ -28,13 +39,6 @@ public class Registro extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //Logica de el boton
-
-                /*Preguntas al Profesor si hacer de nuevo el intent es una buena practica por que
-                es como si se agregara a la pila multiples pantallas de la misma actividad.
-
-                Intent back = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(back);*/
-
                 finish();
             }
         });
@@ -42,10 +46,53 @@ public class Registro extends AppCompatActivity {
         btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Logica de el boton
-                Intent elerol = new Intent(getApplicationContext(), ElecRol.class);
-                startActivity(elerol);
+                // Lógica del botón de registro
+                if (camposLlenos() && isValidEmail(correo.getText().toString()) && contrasenasIguales()) {
+                    // Todos los campos requeridos están llenos, el correo tiene un formato válido,
+                    // y las contraseñas coinciden, puedes avanzar a la siguiente actividad
+                    Intent elerol = new Intent(getApplicationContext(), ElecRol.class);
+                    startActivity(elerol);
+                } else {
+                    // Muestra un mensaje de error apropiado según la condición que no se cumple
+                    if (!contrasenasIguales()) {
+                        Toast.makeText(Registro.this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(Registro.this, "Completa todos los campos requeridos y verifica el formato del correo", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
 
+
+
+            private boolean contrasenasIguales() {
+                String contrasenaText = contrasena.getText().toString();
+                String concontrasenaText = confContrasena.getText().toString();
+
+                // Verifica si las contraseñas coinciden
+                return contrasenaText.equals(concontrasenaText);
+            }
+
+            private boolean camposLlenos() {
+                String nombreText = nombre.getEditText().getText().toString();
+                String apellidopText = apellidop.getEditText().getText().toString();
+                String apellidomText = apellidom.getEditText().getText().toString();
+                String contrasenaText = contrasena.getText().toString();
+
+                String concontrasenaText = confContrasena.getText().toString();
+                String correoText = correo.getText().toString();
+
+                // Verifica si los campos requeridos están llenos
+                return !TextUtils.isEmpty(nombreText) &&
+                        !TextUtils.isEmpty(apellidopText) &&
+                        !TextUtils.isEmpty(apellidomText) &&
+                        !TextUtils.isEmpty(contrasenaText) &&
+                        !TextUtils.isEmpty(concontrasenaText) &&
+                        !TextUtils.isEmpty(correoText);
+            }
+
+            private boolean isValidEmail(String email) {
+                // Puedes implementar aquí una validación más completa para el formato de correo
+                return email.contains("@");
             }
         });
 
